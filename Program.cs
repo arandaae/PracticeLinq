8e5d97dfc71be6c847db46a3785b7583268eea91
Armando Aranda <arandaae19@gmail.com>
2022-05-11 08:38:22 -0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PracticeLinq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> videoGameNames = new List<string>
+            {
+                "Metal Gear Solid",
+                "Halo",
+                "The Last of Us",
+                "Inside",
+            };
+
+            var games = videoGameNames.Where(x => x.Length > 0).OrderBy(x => x.Length);
+            foreach (var videoGame in games)
+            {
+                Console.WriteLine(videoGame);
+            }
+        }
+    }
+}
